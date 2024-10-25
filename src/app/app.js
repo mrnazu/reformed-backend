ba71bc169b0565b33a161e2e973a2ac8b7916623
mrnazu <samuelamsalu1998@gmail.com>
2024-10-25 08:47:29 -0700

    admin article route

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const adminRouter = require('../routes/admin/adminAuthRoute');
+const adminArticleRouter = require('../routes/admin/adminArticlesRoute');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 //routes
 app.use('/api/v1/admin', adminRouter);
+app.use('/api/v1/admin', adminArticleRouter);
 
 
 module.exports = app;
--- src/routes/admin/adminArticlesRoute.js
+++ src/routes/admin/adminArticlesRoute.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const adminArticleRouter = express.Router();
 
-adminArticleRouter.get('/articles');
-adminArticleRouter.get('/articles/:id');
-adminArticleRouter.post('/articles');
-adminArticleRouter.put('/articles/:id');
-adminArticleRouter.delete('/articles/:id');
+// Fetch all articles
+adminArticleRouter.get('/articles', (req, res) => {
+  res.status(200).json({ message: 'retrieve all articles' });
+});
 
-// Article Categories
-adminArticleRouter.get('/article-categories');
-adminArticleRouter.post('/article-categories');
+// Fetch a single article by ID
+adminArticleRouter.get('/articles/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ message: `retrieve article with ID: ${id}` });
+});
+
+// Create a new article
+adminArticleRouter.post('/articles', (req, res) => {
+  res.status(201).json({ message: 'create a new article' });
+});
+
+// Update an existing article by ID
+adminArticleRouter.put('/articles/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ message: `update article with ID: ${id}` });
+});
+
+// Delete an article by ID
+adminArticleRouter.delete('/articles/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ message: `delete article with id: ${id}` });
+});
+
+// Fetch all article categories
+adminArticleRouter.get('/article-categories', (req, res) => {
+  res.status(200).json({ message: 'retrieve all article categories' });
+});
+
+// Create a new article category
+adminArticleRouter.post('/article-categories', (req, res) => {
+  res.status(201).json({ message: 'create a new article category' });
+});
+
+module.exports = adminArticleRouter;
