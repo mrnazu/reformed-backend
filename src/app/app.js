6de118a59e5798cb5a791625c276a5a8f3759063
mrnazu <samuelamsalu1998@gmail.com>
2024-10-25 08:58:15 -0700

    admin devotionals router w refactor

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const adminRouter = require('../routes/admin/adminAuthRoute');
-const adminArticleRouter = require('../routes/admin/adminArticlesRoute');
-const adminBooksRouter = require('../routes/admin/adminBooksRouter');
-const adminConfessionsRouter = require('../routes/admin/adminConfessionsRoute');
-const adminCreedRouter = require('../routes/admin/adminCreedRoute');
-const adminDevotionalsRouter = require('../routes/admin/adminDevotionalsRoute');
+const express = require("express");
+const adminRouter = require("../routes/admin/adminAuthRoute");
+const adminArticleRouter = require("../routes/admin/adminArticlesRoute");
+const adminBooksRouter = require("../routes/admin/adminBooksRouter");
+const adminConfessionsRouter = require("../routes/admin/adminConfessionsRoute");
+const adminCreedRouter = require("../routes/admin/adminCreedRoute");
+const adminDevotionalsRouter = require("../routes/admin/adminDevotionalsRoute");
 
 const app = express();
 
@@ -12,12 +12,11 @@ const app = express();
 app.use(express.json());
 
 //routes
-app.use('/api/v1/admin', adminRouter);
-app.use('/api/v1/admin', adminArticleRouter);
-app.use('/api/v1/admin', adminBooksRouter);
-app.use('/api/v1/admin', adminConfessionsRouter);
-app.use('/api/v1/admin', adminCreedRouter);
-app.use('/api/v1/admin', adminDevotionalsRouter);
-
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/admin", adminArticleRouter);
+app.use("/api/v1/admin", adminBooksRouter);
+app.use("/api/v1/admin", adminConfessionsRouter);
+app.use("/api/v1/admin", adminCreedRouter);
+app.use("/api/v1/admin", adminDevotionalsRouter);
 
 module.exports = app;
--- src/routes/admin/adminDevotionalsRoute.js
+++ src/routes/admin/adminDevotionalsRoute.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const adminDevotionalsRouter = express.Router();
 
 // Fetch all devotionals
-adminDevotionalsRouter.get('/devotionals', (req, res) => {
-  res.status(200).json({ message: 'retrieve all devotionals' });
+adminDevotionalsRouter.get("/devotionals", (req, res) => {
+  res.status(200).json({ message: "retrieve all devotionals" });
 });
 
 // Fetch a single devotional by ID
-adminDevotionalsRouter.get('/devotionals/:id', (req, res) => {
+adminDevotionalsRouter.get("/devotionals/:id", (req, res) => {
   const { id } = req.params;
   res.status(200).json({ message: `retrieve devotional with id: ${id}` });
 });
 
 // Create a new devotional
-adminDevotionalsRouter.post('/devotionals', (req, res) => {
-  res.status(201).json({ message: 'create a new devotional' });
+adminDevotionalsRouter.post("/devotionals", (req, res) => {
+  res.status(201).json({ message: "create a new devotional" });
 });
 
 // Update an existing devotional by ID
-adminDevotionalsRouter.put('/devotionals/:id', (req, res) => {
+adminDevotionalsRouter.put("/devotionals/:id", (req, res) => {
   const { id } = req.params;
   res.status(200).json({ message: `Update devotional with id: ${id}` });
 });
 
 // Delete a devotional by ID
-adminDevotionalsRouter.delete('/devotionals/:id', (req, res) => {
+adminDevotionalsRouter.delete("/devotionals/:id", (req, res) => {
   const { id } = req.params;
   res.status(200).json({ message: `delete devotional with id: ${id}` });
 });
