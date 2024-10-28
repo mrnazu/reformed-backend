ab62dc101556c259a7ca2e7c8e597ad5d8e7128d
mrnazu <samuelamsalu1998@gmail.com>
2024-10-28 09:46:07 -0700

    admin route

--- src/app/app.js
+++ src/app/app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const adminRouter = require("../routes/admin/adminAuthRoute");
 const adminArticleRouter = require("../routes/admin/adminArticlesRoute");
 const adminBooksRouter = require("../routes/admin/adminBooksRouter");
-const adminConfessionsRouter = require("../routes/admin/adminConfessionsRoute");
-const adminCreedRouter = require("../routes/admin/adminCreedRoute");
-const adminDevotionalsRouter = require("../routes/admin/adminDevotionalsRoute");
 
 const app = express();
 
@@ -12,11 +9,8 @@ const app = express();
 app.use(express.json());
 
 // Admin Routes
-app.use("/api/v1/admin/auth", adminRouter); // Authentication routes
-app.use("/api/v1/admin/articles", adminArticleRouter); // Article routes
-app.use("/api/v1/admin/books", adminBooksRouter); // Book routes
-app.use("/api/v1/admin/confessions", adminConfessionsRouter); // Confession routes
-app.use("/api/v1/admin/creeds", adminCreedRouter); // Creed routes
-app.use("/api/v1/admin/devotionals", adminDevotionalsRouter); // Devotional routes
+app.use("/api/v1/admin/auth", adminRouter);
+app.use("/api/v1/admin/articles", adminArticleRouter);
+app.use("/api/v1/admin/books", adminBooksRouter);
 
 module.exports = app;
