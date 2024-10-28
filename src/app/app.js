7cf67776915b8cbab41458953e977dab5ae59968
mrnazu <samuelamsalu1998@gmail.com>
2024-10-28 10:19:56 -0700

    user routes

--- src/app/app.js
+++ src/app/app.js
@@ -4,20 +4,26 @@ const adminArticleRouter = require("../routes/admin/adminArticlesRoute");
 const adminBooksRouter = require("../routes/admin/adminBooksRouter");
 const homeRoute = require("../routes/homeRoute");
 const authRoute = require("../routes/authRoute");
+const userBooksRoute = require("../routes/user/userBooksRoute");
+const userConfessionRoute = require("../routes/user/userConfessionsRoute");
+const userCreedRoute = require("../routes/user/userCreedsRoute");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-// Home
+// Home and Auth
 app.use("/", homeRoute);
-
-// Auth
 app.use('/api/v1/auth', authRoute);
 
 // Admin Routes
 app.use("/api/v1/admin/articles", adminArticleRouter);
 app.use("/api/v1/admin/books", adminBooksRouter);
+
+// User Routes
+app.use("/api/v1/user/books", userBooksRoute);
+app.use("/api/v1/user/confessions", userConfessionRoute);
+app.use("/api/v1/user/creeds", userCreedRoute);
 
 module.exports = app;
