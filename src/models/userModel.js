c120c2c7385aa8dbfa7b0bd8be740bb4ec9c54d3
mrnazu <samuelamsalu1998@gmail.com>
2024-11-02 07:24:05 -0700

    user model

---
+++ src/models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userID: {
+      type: String,
+      required: true,
+      default: function () {
+        return (
+          "USR" +
+          Math.floor(100 + Math.random() * 900) +
+          Date.now().toString().slice(2, 4) +
+          this.name
+            .split(" ")
+            .map((name) => name[0])
+            .join("")
+            .toUpperCase()
+        );
+      },
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isSuspended: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports - User;
